88bdb63a972e452b0116f692275e302202cd1989
Anubhav <106881580+its-me-anubhav@users.noreply.github.com>
2022-09-21 19:02:00 +0530

    Create data_types.cpp

---
+++ ASSIGNMENT1/data_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char a;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &i,&l,&a,&f,&d);
+     printf("%d\n%ld\n%c\n%f\n%lf",i,l,a,f,d);
+    return 0;
+}
