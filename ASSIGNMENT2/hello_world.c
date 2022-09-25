801dea448e8a1961dbc5ad8d14cb75cb1c5a4174
Anubhav <106881580+its-me-anubhav@users.noreply.github.com>
2022-09-25 09:20:05 +0530

    Create hello_world.c

---
+++ ASSIGNMENT2/hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    printf("Hello, World!\n");
+    printf("Welcome to C programming.");
+    return 0;
+}
