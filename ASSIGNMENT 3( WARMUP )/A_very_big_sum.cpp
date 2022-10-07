9ee73cc3226a3c1254bc9d077aaf69d4c3cf3035
Anubhav <106881580+its-me-anubhav@users.noreply.github.com>
2022-10-07 08:20:50 +0530

    Create A_very_big_sum.cpp

---
+++ ASSIGNMENT 3( WARMUP )/A_very_big_sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+int n;
+long long int sum=0;
+cin >> n;
+vector<int> arr(n);
+for(int arr_i = 0;arr_i < n;arr_i++){
+cin >> arr[arr_i];
+sum+=arr[arr_i];
+}
+cout<<sum;
+return 0;
+}
